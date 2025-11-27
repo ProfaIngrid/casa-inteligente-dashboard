02cbfcb2cbd291e5e3e4d1dba58ba0627a2d3057
ProfaIngrid <ingrid.bconceicao@senacsp.edu.br>
2025-11-27 09:40:42 -0300

    conexao

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ const MQTT_URL = "wss://test.mosquitto.org:8081";
 const clienteId = 'web-casa-inteligente-ingrid';
 
 // ### Criando o cliente MQTT do navegador, POREM ainda não estamos conectados ao BROKER
-// const cliente = mqtt.connect(MQTT_URL, {
-//     clienteId,
-//     clean: true,
-//     connectTimeout: 4000,
-// });
+const cliente = mqtt.connect(MQTT_URL, {
+    clienteId,
+    clean: true,
+    connectTimeout: 4000,
+});
 
 // ################# Variaveis dos dispositivos
 const lampSalaInput = document.getElementById("lamp-sala");
@@ -139,32 +139,32 @@ irVarandaInput.addEventListener("change", () => {
 
 // ### chamando um evento do JS do tipo "DOMContentLoaded", que é o evento que acontece 
 //      após toda a minha página de HTML ser carregada
-// document.addEventListener("DOMContentLoaded", () => {
-//     console.log("Página carregada com sucesso ✅... Conectando ao Mosquitto!");
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Página carregada com sucesso ✅... Conectando ao Mosquitto!");
 
-//     // ### Estabelecendo a conexão com o broker mqtt
-//     cliente.on("connect", () => {
-//         console.log("Conexão estabelecida com Sucesso ✅!");
-//         console.log("Cliente conectado: ", clienteId);
+    // ### Estabelecendo a conexão com o broker mqtt
+    cliente.on("connect", () => {
+        console.log("Conexão estabelecida com Sucesso ✅!");
+        console.log("Cliente conectado: ", clienteId);
 
-//         // ### criando um topico para acessar mensagens do MQTT
-//         const topicoTeste = 'teste/ingrid';
+        // ### criando um topico para acessar mensagens do MQTT
+        const topicoTeste = 'teste/ingrid';
 
-//         // ### recebendo mensagens do topico criado
-//         cliente.subscribe(topicoTeste);
-//     });
+        // ### recebendo mensagens do topico criado
+        cliente.subscribe(topicoTeste);
+    });
 
-//     // ### Preparando mensagem de erro caso algo aconteça
-//     cliente.on("error", (erro) => {
-//         console.error("Erro ao conectar ao Broker MQTT 🚫!");
-//         console.error(erro);
-//     });
+    // ### Preparando mensagem de erro caso algo aconteça
+    cliente.on("error", (erro) => {
+        console.error("Erro ao conectar ao Broker MQTT 🚫!");
+        console.error(erro);
+    });
 
-//     // ### Recebendo as mensagem dos tópicos assinados no MQTT pelo cliente
-//     cliente.on("message", (topico, mensagem) => {
-//         console.log("Topico recebido: ", topico);
-//         console.log("Mensagem recebida: ", mensagem);
-//     });
-// });
+    // ### Recebendo as mensagem dos tópicos assinados no MQTT pelo cliente
+    cliente.on("message", (topico, mensagem) => {
+        console.log("Topico recebido: ", topico);
+        console.log("Mensagem recebida: ", mensagem);
+    });
+});
 
 
